0768b65a2997b51350205c98ab66cdb57fd61ae3
Miguel319 <miguelp90.319@gmail.com>
2019-12-03 22:31:25 -0400

    Heroku commit 5

--- server.js
+++ server.js
@@ -6,6 +6,4 @@ app.use(express.static(`${__dirname}/dist/ng-cnews`));
 
 app.listen(process.env.PORT || 8080);
 
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(`${__dirname}/dist/ng-cnews/index.html`))
-);
+app.get("/*", (req, res) => res.sendFile(path.join(`${__dirname}/dist/ng-cnews/index.html`)));
