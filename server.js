90000c734241dd27f133a4f16bb56abceb23395d
Miguel319 <miguelp90.319@gmail.com>
2019-12-03 22:08:16 -0400

    Heroku commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', (req, res) => res.sendFile(path.join(`${__dirname}/dist/index.html`)));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
